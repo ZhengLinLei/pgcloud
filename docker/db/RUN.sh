73d05265c2b5522ce02963ff9746a0811d3cef5a
Zheng Lin Lei <zheng9112003@gmail.com>
2024-08-09 16:19:27 +0200

    perf: Added failover logic

--- docker/db/RUN.sh
+++ docker/db/RUN.sh
@@ -3,14 +3,29 @@
 IMAGE_NAME=pgcloud_db
 CONTAINER_NAME=pgcloud_db
 
+# -- Linux interface docker0: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500
+# docker run  -it --rm --network=host           \
+#             --name $CONTAINER_NAME            \
+#             --env-file ./.env                 \
+#             -v /etc/localtime:/etc/localtime  \
+#             -v pgdata:/opt/pgdata             \
+#             -v ./.env:/opt/pgcloud/.env       \
+#             $IMAGE_NAME
 
-docker run  -it --rm --network=host           \
+# -- Mac OS interface docker0: flags=8843<UP,BROADCAST,RUNNING,SIMPLEX,MULTICAST> mtu 1500
+# Make sure to change expose port if you want to change the Postgres port in .env
+docker run  -it --rm                          \
             --name $CONTAINER_NAME            \
+            -p 5432:5432                      \
             --env-file ./.env                 \
             -v /etc/localtime:/etc/localtime  \
             -v pgdata:/opt/pgdata             \
             -v ./.env:/opt/pgcloud/.env       \
             $IMAGE_NAME
+
+# If you are running in same machine, and want to use second instance in MacOS / WIndow. Include
+# host.docker.internal as 127.0.0.1 lp IP
+
 
 docker exec -it $CONTAINER_NAME bash
 
