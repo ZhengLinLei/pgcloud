919aed50976271462c06d5671c3ed0ca4bf5ffe8
Zheng Lin Lei <zheng9112003@gmail.com>
2024-09-21 13:46:50 +0200

    Updated PgPool to > 4.3 with Ubuntu 24.04

--- static/pgpool/sbin/entrypoint.sh
+++ static/pgpool/sbin/entrypoint.sh
@@ -75,6 +75,75 @@ else
     set +a
     pglog "---------------"
 
+    pglog "----- Generating PgPool Cluster conf ------"
+    # Reading all files starting from server*.env
+    file_count=$(ls -1 $BACKEND_CONF_PATH | grep -E 'backend[0-9]+\.env' | wc -l)
+    pglog "$file_count PgPool servers configuration file found"
+
+    # Iterate from server config files
+    set -a
+    for i in $(seq 0 $((file_count - 1))); do
+        echo "---------------"
+        # Config servers
+        server_file="$BACKEND_CONF_PATH/backend$i.env"
+        pglog "Configurating params for PgPool server $server_file"
+
+        # Source and update data
+        source $server_file || {
+            pglog "Cannot load configuration, please check the logs and fix the error"
+            pglog "Exiting configuration and stop launching pgpool"
+            exit 1
+        }
+
+        if [ "$NODE_PGPOOL_HOST" = "$PGPOOL_HOST" ]; then
+            # Create pgpool_node_id file
+            # Pgpool-II 4.1 or earlier, because it is required to specify its own pgpool node information and 
+            # the destination pgpool nodes information, the settings are different per pgpool node. 
+            # Since Pgpool-II 4.2, all configuration parameters are identical on all hosts. If watchdog feature is enabled, 
+            # to distinguish which host is which, a pgpool_node_id file is required. You need to create a pgpool_node_id 
+            # file and specify the pgpool (watchdog) node number (e.g. 0, 1, 2 ...) to identify pgpool (watchdog) host.
+            #
+            # Example. pgpool_node_id configuration
+            #If you have 3 pgpool nodes with hostname server1, server2 and server3, create the pgpool_node_id file on each host as follows. 
+
+            # [server1]# cat /etc/pgpool2/pgpool_node_id
+            # 0
+                    
+
+            # [server2]# cat /etc/pgpool2/pgpool_node_id
+            # 1
+                    
+
+            # [server3]# cat /etc/pgpool2/pgpool_node_id
+            # 2
+
+            echo $i > /etc/pgpool2/pgpool_node_id
+
+            pglog "Node id detected for this node: $1"
+        fi
+
+        pglog "hostname$i           = $NODE_PGPOOL_HOST"
+        pglog "wd_port$i            = $NODE_WD_PORT"
+        pglog "pgpool_port$i        = $NODE_PGPOOL_PORT"
+        pglog "Heartbeat"
+        pglog "heartbeat_hostname$i = $NODE_PGPOOL_HOST"
+        pglog "heartbeat_port$i     = $NODE_HEARTBEAT_PORT"
+        pglog "heartbeat_device$i   = $NODE_HEARTBEAT_DEVICE"
+        pglog ""
+
+        echo "hostname$i           = $NODE_PGPOOL_HOST"         >> $CONF_BASE_PATH/pgpool.conf
+        echo "wd_port$i            = $NODE_WD_PORT"             >> $CONF_BASE_PATH/pgpool.conf
+        echo "pgpool_port$i        = $NODE_PGPOOL_PORT"         >> $CONF_BASE_PATH/pgpool.conf
+        echo "heartbeat_hostname$i = $NODE_PGPOOL_HOST"         >> $CONF_BASE_PATH/pgpool.conf
+        echo "heartbeat_port$i     = $NODE_HEARTBEAT_PORT"      >> $CONF_BASE_PATH/pgpool.conf
+        echo "heartbeat_device$i   = $NODE_HEARTBEAT_DEVICE"    >> $CONF_BASE_PATH/pgpool.conf 
+        echo ""                                                 >> $CONF_BASE_PATH/pgpool.conf
+            
+        
+    done
+    set +a
+    pglog "---------------"
+
     pglog "----- Generating Global conf ------"
     # Global configuration
     pglog "Configurating global parameters"
@@ -117,6 +186,29 @@ else
     pglog " Pass: $PCP_PASS"
     echo -e "\n$PCP_USER:$(pg_md5 $PCP_PASS)" >> $CONF_BASE_PATH/pcp.conf
 
+
+    # Watchdog configuration PGPOOL_NUM = 1 disable, PGPOOL_NUM > 1 enable
+    pglog "----- Generating Watchdog conf ------"
+    echo -e "\n# WATCHDOG CONFIG"               >> $CONF_BASE_PATH/pgpool.conf
+    if [ "$PGPOOL_NUM" = "1" ]; then
+        pglog "Watchdog disabled"
+        echo "use_watchdog = off"               >> $CONF_BASE_PATH/pgpool.conf
+    else 
+        pglog "Watchdog enabled"
+        echo "use_watchdog = on"                >> $CONF_BASE_PATH/pgpool.conf
+
+        # Watchdog configuration
+        pglog "Configurating watchdog parameters"
+        pglog "wd_lifecheck_method      = 'heartbeat'"
+        pglog "wd_interval              = $WD_HEARTBEAT_INTERVAL"
+        pglog "wd_heartbeat_keepalive   = $WD_HEARTBEAT_KEEPALIVE"
+        pglog "wd_heartbeat_deadtime    = $WD_HEARTBEAT_DEADTIME"
+
+        echo "wd_lifecheck_method      = 'heartbeat'"              >> $CONF_BASE_PATH/pgpool.conf
+        echo "wd_interval              = $WD_HEARTBEAT_INTERVAL"   >> $CONF_BASE_PATH/pgpool.conf
+        echo "wd_heartbeat_keepalive   = $WD_HEARTBEAT_KEEPALIVE"  >> $CONF_BASE_PATH/pgpool.conf
+        echo "wd_heartbeat_deadtime    = $WD_HEARTBEAT_DEADTIME"   >> $CONF_BASE_PATH/pgpool.conf
+    fi
 
     # Move to config path
     cp -rfa $CONF_BASE_PATH/. $PGPOOL_CONF_PATH
